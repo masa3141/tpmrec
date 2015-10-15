c5386b98717787033b1ede404a2f9404db0d6839
mk <masahiro@kazama.tv>
2015-10-15 23:38:01 +0900

    modified name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    name='tmpmrec',
+    name='tpmrec',
     version='1.0.0',
     description='A flexible framework of topic model and recommender system',
     packages=['tpmrec'],
---
+++ tests/test_nb.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from tpmrec import nb
+
+
+class TestNb(unittest.TestCase):
+    def setUp(self):
+        super(TestNb, self).setUp()
+        
